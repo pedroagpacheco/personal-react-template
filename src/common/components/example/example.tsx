40f2eb2c78ac663adfb92415196e57bb310caa1c
pedroagpacheco <29712227+pedroagpacheco@users.noreply.github.com>
2023-04-11 02:01:14 -0300

    feat: added tailwind package

--- src/common/components/example/example.tsx
+++ src/common/components/example/example.tsx
@@ -1,16 +1,13 @@
 import './example.scss'
 
 const App = () => (
-  <>
-    <h1>Checklist</h1>
-    <ol>
+  <div className="p-4">
+    <h1 className="text-4xl font-bold pb-4">Checklist</h1>
+    <ul>
       <li>react-router</li>
-      <li>ESLint</li>
-      <li>Prettier</li>
-      <li>Tailwind</li>
       <li>axe + eslint-plugin-jsx-a11y</li>
-    </ol>
-  </>
+    </ul>
+  </div>
 )
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,4 @@
-import React from 'react'
 import { createRoot } from 'react-dom/client'
-
 import { Example } from 'common/components/example'
 
 import 'core-js/features/array/flat-map'
@@ -10,6 +8,9 @@ import 'core-js/features/set'
 import 'raf/polyfill'
 import 'whatwg-fetch'
 
+import './style.scss'
+
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const container = document.getElementById('app-root')!
 const root = createRoot(container)
 root.render(<Example />)
