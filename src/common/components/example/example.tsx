9844c4e53f07d596555f58af775c12c8f853107c
pedroagpacheco <29712227+pedroagpacheco@users.noreply.github.com>
2023-04-11 00:32:57 -0300

    initial commit

---
+++ src/common/components/example/example.tsx
@@ -0,0 +1,16 @@
+import './example.scss'
+
+const App = () => (
+  <>
+    <h1>Checklist</h1>
+    <ol>
+      <li>react-router</li>
+      <li>ESLint</li>
+      <li>Prettier</li>
+      <li>Tailwind</li>
+      <li>axe + eslint-plugin-jsx-a11y</li>
+    </ol>
+  </>
+)
+
+export default App
